25ed0a730a16ec2b65196a35916b60334fa9493f
saucelux <81427539+saucelux@users.noreply.github.com>
2021-06-11 21:29:22 +0300

    Update auth_test.py

--- auth_test.py
+++ auth_test.py
@@ -3,7 +3,7 @@ import pytest
 import sys
 def inc():
     subprocess.Popen([sys.executable, dev.py]
-    return sys.stdin.read()
+    return sys.stdout.read()
                      
 def test_answer():
     assert inc() == "Hello world from dev"
